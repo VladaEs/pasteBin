440bab8e8fd2b705cbcb6257bb377cdecaf5af92
VladaEs <vooronin2005@gmail.com>
2025-01-01 23:51:29 +0000

    temporary  change

--- frontend/components/workSpace/formPaste/FormPaste.js
+++ frontend/components/workSpace/formPaste/FormPaste.js
@@ -48,7 +48,7 @@ const FormPaste =({getRegister})=>{
             <FormRowWrapper>
                 <div className="text-white flex-1"></div>
                 <InputWrapper>
-                    <input type='submit' value={"submit"} className=" w-7/12 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"/>
+                    <input type='submit' value={"submit"} className=" w-7/12 bg-transparent cursor-pointer hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"/>
                 </InputWrapper>
             </FormRowWrapper>
         </form>
