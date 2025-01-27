d521adafdceca9fa855aa906d1fe0814298f4a1f
VladaEs <vooronin2005@gmail.com>
2025-01-27 00:24:31 +0000

    laravel tailwindcss

---
+++ backend/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PasteBin made By VladaEs</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="pageWrapper mb-5 flex flex-col">
+<x-header />
+
+</body>
+</html>
