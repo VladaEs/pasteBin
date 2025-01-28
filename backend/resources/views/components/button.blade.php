22f9b1bc79e84a8e36b8df55bd77c6411ecdff50
VladaEs <vooronin2005@gmail.com>
2025-01-28 22:50:41 +0000

    form styling is done

---
+++ backend/resources/views/components/button.blade.php
@@ -0,0 +1,15 @@
+@props(['color'=>"blue"])
+
+<button  {{$attributes->class([
+
+    'text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' => $color === 'blue',
+    'text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800' => $color === 'green',
+    'text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900' => $color === 'red',
+    'text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900' => $color === 'yellow',
+    'text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900' => $color === 'purple',
+])->merge([
+    'type'=>"button"
+    ])}}>
+
+    {{$slot}}
+</button>
--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,73 +9,141 @@
     @vite('resources/css/app.css')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
+
 <body>
-<div class="pageWrapper mb-5 flex flex-col">
-    <x-header />
-    <div class='spaceSide'>
-        <x-workspace>
-            <x-gridlayoutform>
-                <div>
-                    <x-textfield>
-                        {{__("New Paste")}}
-                    </x-textfield>
-                    <x-textarea placeholder="Type your message here" id="TextArea" rows='5' />
-                    <x-textfield rows='10'>
-                        {{__("Optional Paste Settings")}}
-                    </x-textfield >
-                    <hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+    <div class="pageWrapper mb-5 flex flex-col">
+        <x-header />
+        <div class='spaceSide'>
+            <x-workspace>
+                <x-gridlayoutform>
+                    <div>
+                        <x-textfield>
+                            {{ __('New Paste') }}
+                        </x-textfield>
+                        <x-textarea placeholder="Type your message here" id="TextArea" rows='5' />
+                        <x-textfield rows='10'>
+                            {{ __('Optional Paste Settings') }}
+                        </x-textfield>
+                        <hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700">
+                        </hr>
 
 
-                    <x-form.form-paste action="" method="POST" id="pasteForm" onkeydown="return event.key != 'Enter';">
-                        {{-- Select --}}
-                        <x-form.form-row-wrapper>
-                            <div class="text-white flex-1"><x-form.paste-option-span>{{__("Category")}}</x-form.paste-option-span></div>
-                            <x-form.input-wrapper>
-                                <x-form.form-select>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                    <option>6</option>
-                                    <option>7</option>
-                                </x-form-select>
-                            </x-form.input-wrapper>
-                        </x-form.form-row-wrapper>
-                        {{-- Select --}}
+                        <x-form.form-paste action="" method="POST" id="pasteForm"
+                            onkeydown="return event.key != 'Enter';">
+                            {{-- Select Category--}}
+                            <x-form.form-row-wrapper>
+                                <div class="text-white flex-1">
+                                    <x-form.paste-option-span>{{ __('Category') }}</x-form.paste-option-span></div>
+                                <x-form.input-wrapper>
+                                    <x-form.form-select name="Category">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        </x-form-select>
+                                </x-form.input-wrapper>
+                            </x-form.form-row-wrapper>
+                            {{-- Select Category --}}
 
-                         {{-- tags --}}
-                         <x-form.form-row-wrapper>
-                            <div class="text-white flex-1"><x-form.paste-option-span>{{__("Tags")}}</x-form.paste-option-span></div>
-                            <x-form.input-wrapper>
-                                <div id="tags-container" class="w-full gap-5 bg-gray-50 border border-gray-300 dark:text-neutral-400 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-neutral-900 dark:border-neutral-700">
-                                    <ul id="tags-list" class="flex flex-row flex-wrap"></ul>
-                                    <input type="text" id="tags-input" class="inputField inputFieldTags" >
-                                    <input type="hidden" id="hidden-field-tags">
+                            {{-- tags --}}
+                            <x-form.form-row-wrapper>
+                                <div class="text-white flex-1">
+                                    <x-form.paste-option-span>{{ __('Tags') }}</x-form.paste-option-span></div>
+                                <x-form.input-wrapper>
+                                    <div id="tags-container"
+                                        class="w-full gap-5 bg-gray-50 border border-gray-300 dark:text-neutral-400 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-neutral-900 dark:border-neutral-700">
+                                        <ul id="tags-list" class="flex flex-row flex-wrap tagsList"></ul>
+                                        <input type="text" id="tags-input" class="inputField inputFieldTags">
+                                        <input type="hidden" id="hidden-field-tags">
+                                    </div>
+
+
+
+
+                                </x-form.input-wrapper>
+                            </x-form.form-row-wrapper>
+                            {{-- tags --}}
+
+                            {{-- Select Expiration--}}
+                            <x-form.form-row-wrapper>
+                                <div class="text-white flex-1">
+                                    <x-form.paste-option-span>{{ __('Paste Expiration') }}</x-form.paste-option-span></div>
+                                <x-form.input-wrapper>
+                                    <x-form.form-select name="expiration">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        </x-form-select>
+                                </x-form.input-wrapper>
+                            </x-form.form-row-wrapper>
+                            {{-- Select Expiration --}}
+
+
+                            {{-- Select privacy--}}
+                            <x-form.form-row-wrapper>
+                                <div class="text-white flex-1">
+                                    <x-form.paste-option-span>{{ __('Paste Privacy') }}</x-form.paste-option-span></div>
+                                <x-form.input-wrapper>
+                                    <x-form.form-select name="privacy">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        </x-form-select>
+                                </x-form.input-wrapper>
+                            </x-form.form-row-wrapper>
+                            {{-- Select privacy --}}
+
+
+                            {{-- input password --}}
+                            <x-form.form-row-wrapper>
+                                <div class="text-white flex-1">
+                                    <x-form.paste-option-span>{{ __('Paste Password') }}</x-form.paste-option-span>
                                 </div>
+                                <x-form.input-wrapper>
+                                    <x-input name="password" type="password" placeholder="Type your password here"/>
+                                </x-form.input-wrapper>
+                            </x-form.form-row-wrapper>
+                            {{-- input password --}}
 
+                            {{-- input name --}}
+                            <x-form.form-row-wrapper>
+                                <div class="text-white flex-1">
+                                    <x-form.paste-option-span>{{ __('Paste Name') }}</x-form.paste-option-span>
+                                </div>
+                                <x-form.input-wrapper>
+                                    <x-input name="pasteName" type="text" placeholder="Type paste name here"/>
+                                </x-form.input-wrapper>
+                            </x-form.form-row-wrapper>
+                            {{-- input name --}}
 
+                            <x-button type="submit" color="blue">
+                                {{__("Create")}}
+                            </x-button>
+                            </x-form-paste>
 
+                    </div>
+                    <x-sidebar>
+                        {{ __('Public Pastes') }}
+                    </x-sidebar>
+                </x-gridlayoutform>
+            </x-workspace>
 
-                            </x-form.input-wrapper>
-                        </x-form.form-row-wrapper>
-                        {{-- tags --}}
-
-
-                    </x-form-paste>
-
-                </div>
-                <x-sidebar>
-                    {{__("Public Pastes")}}
-                </x-sidebar>
-            </x-gridlayoutform>
-        </x-workspace>
-
+        </div>
     </div>
-</div>
 
 
-{{-- <div> {/*section 1 from the right where the text area is*/}
+    {{-- <div> {/*section 1 from the right where the text area is*/}
     <TextFeild>New Paste</TextFeild>
     <TextArea register={reg}/>
     <TextFeild>Optional Paste Settings</TextFeild>
@@ -88,41 +157,54 @@
 </div>
 
  --}}
-<script>
-    /* Text Area resize*/
-    let textArea = document.getElementById("TextArea");
-    textArea.addEventListener('input', function(event){
-        if (textArea) {
-            textArea.style.height = 'auto'; // Reset the height
-            textArea.style.height = `${textArea.scrollHeight}px`; // Set the height
-    }
-    });
+    <script>
+        /* Text Area resize*/
+        let textArea = document.getElementById("TextArea");
+        textArea.addEventListener('input', function(event) {
+            if (textArea) {
+                textArea.style.height = 'auto'; // Reset the height
+                textArea.style.height = `${textArea.scrollHeight}px`; // Set the height
+            }
+        });
 
 
-//-=-=-=-=-=-=-=-=-=-=-=-=-=tags -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-let tags= [];
-let hiddenInput =document.getElementById('hidden-field-tags');
-$('#tags-container').on('click', function(){
-    $('.inputFieldTags').focus();
-});
-$("#tags-input").on('keydown', function(event){
-    if(event.key =="Enter"){
-        console.log("saved: "+ event.target.value);
-    }
-    else{
-        console.log(event.target.value);
-    }
-
-});
-
-
-
-
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=tags -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-</script>
+        //-=-=-=-=-=-=-=-=-=-=-=-=-=tags -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        let tags = [];
+        let hiddenInput = document.getElementById('hidden-field-tags');
+        $('#tags-container').on('click', function() {
+            $('.inputFieldTags').focus();
+        });
+        function drawTags(arr, selector = "tags-list") {
+            if (!(Array.isArray(arr)) && !(arr.length >= 1)) return;
+            let wrapper = document.getElementById(selector);
+            wrapper.innerHTML = "";
+            let classArr= ['cursor-pointer'];
+            arr.forEach((tag, ind) => {
+                const tagItem = document.createElement('li');
+                tagItem.textContent = tag;
+                classArr.forEach(item=>{tagItem.classList.add(item);});
+                tagItem.setAttribute('data-index', ind);
+                tagItem.addEventListener('click', function(event){
+                    let indexDelete =parseInt(this.getAttribute('data-index'), 10);
+                    if (indexDelete < 0 || indexDelete >= tags.length) return;
+                    tags.splice(parseInt(indexDelete), 1);
+                    drawTags(tags);
+                });
+                wrapper.appendChild(tagItem);
+            });
+        }
+        $("#tags-input").on('keydown', function(event) {
+            if (event.key == "Enter") {
+                if(event.target.value.length <= 1) return;
+                tags.push(event.target.value);
+                drawTags(tags);
+                this.value= "";
+            }
+        });
+        //-=-=-=-=-=-=-=-=-=-=-=-=-=tags -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    </script>
 
 
 </body>
+
 </html>
