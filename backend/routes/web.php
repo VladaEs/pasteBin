47381ca92080a5447a30eb46e725b7019b7856c1
VladaEs <tookens2005@gmail.com>
2025-03-25 21:26:48 +0000

    breeze installed

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\formController;
 use App\Http\Controllers\pasteController;
 use App\Http\Controllers\pastePassController;
 use App\Http\Controllers\Auth;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', [formController::class, "index"])->name('home');
 Route::post('/paste/store', [formController::class, "store"])->name("createPaste");
@@ -42,4 +43,16 @@ Route::post('/register', [Auth::class, 'create'])->name('createAccount');
 
 
 
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
