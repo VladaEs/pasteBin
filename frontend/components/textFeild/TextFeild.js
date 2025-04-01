630ff6f5408d0005d979f4a8714b5806af610aef
VladaEs <vooronin2005@gmail.com>
2025-04-01 18:45:09 +0100

    deleted frontend

--- frontend/components/textFeild/TextFeild.js
+++
@@ -1,9 +0,0 @@
-const TextFeild =(props)=>{
-
-    return(
-        <div className={`text-l  my-2 uppercase font-sans font-bold `}>{props.children}</div>
-
-    )
-}
-
-export default TextFeild;
--- frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env:{
-        API_PATH:"http://localhost:8000/api",
-    }
-};
-
-export default nextConfig;
